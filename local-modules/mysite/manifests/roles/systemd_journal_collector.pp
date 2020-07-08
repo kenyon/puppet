2e39fb45910285cbabd4ed50f41ba05b086dc5a1
Kenyon Ralph <kenyon@kenyonralph.com>
2020-07-07 22:04:12 -0700

    have systemd-journal-remote listen on http instead of https
    For now.

--- local-modules/mysite/manifests/roles/systemd_journal_collector.pp
+++ local-modules/mysite/manifests/roles/systemd_journal_collector.pp
@@ -11,4 +11,10 @@ class mysite::roles::systemd_journal_collector {
     enable  => true,
     require => Package['systemd-journal-remote'],
   }
+
+  systemd::dropin_file { 'systemd-journal-remote.conf':
+    unit    => 'systemd-journal-remote.service',
+    source => "puppet:///modules/${module_name}/systemd-journal-remote.conf",
+    notify  => Service['systemd-journal-remote'],
+  }
 }
