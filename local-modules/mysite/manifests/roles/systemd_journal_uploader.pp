1e1d937d5011ef6ddc232c9c6e383e642db9efc2
Kenyon Ralph <kenyon@kenyonralph.com>
2020-07-24 22:32:22 -0700

    systemd_journal_uploader: enable and configure service

--- local-modules/mysite/manifests/roles/systemd_journal_uploader.pp
+++ local-modules/mysite/manifests/roles/systemd_journal_uploader.pp
@@ -5,4 +5,19 @@ class mysite::roles::systemd_journal_uploader {
     ['systemd-journal-remote'],
     {ensure => installed},
   )
+
+  service { 'systemd-journal-upload':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['systemd-journal-remote'],
+  }
+
+  ini_setting { 'systemd_journal_upload_url':
+    ensure  => 'present',
+    path    => '/etc/systemd/journal-upload.conf',
+    section => 'Upload',
+    setting => 'URL',
+    value   => 'http://journal.kenyonralph.com',
+    notify  => Service['systemd-journal-upload'],
+  }
 }
