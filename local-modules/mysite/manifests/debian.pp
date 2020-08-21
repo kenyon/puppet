217a8ecef562100ed5c2e031224ca29cf068fcd2
Kenyon Ralph <kenyon@kenyonralph.com>
2020-08-21 11:56:15 -0700

    debian.pp: remove resource collectors that can cause dependency cycles
    These resource collectors cause dependency cycles with the
    unattended_upgrades module that I'm migrating to. I don't think these
    resource collectors are actually necessary. At worst, two puppet runs
    should handle situations where packages want to be installed that
    depend on some apt configuration that hasn't been applied yet.

--- local-modules/mysite/manifests/debian.pp
+++ local-modules/mysite/manifests/debian.pp
@@ -2,8 +2,4 @@
 # Configuration common to Debian-family Linux machines.
 
 class mysite::debian {
-  # Ensure that apt is configured before any package
-  # activities can happen.
-  Apt::Conf <| |> -> Package <| |>
-  Apt::Setting <| |> -> Package <| |>
 }
