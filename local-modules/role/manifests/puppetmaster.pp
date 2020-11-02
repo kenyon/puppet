4aa5cc6ca11bc9319deca9fdb4866dcea4e4961e
Kenyon Ralph <kenyon@kenyonralph.com>
2020-11-01 22:48:23 -0800

    refactor puppetmaster role
    Role data go in the role module or role fact hiera data, node-specific
    data stay in the node data file.

--- local-modules/role/manifests/puppetmaster.pp
+++ local-modules/role/manifests/puppetmaster.pp
@@ -1,21 +1,62 @@
 # -*- coding: utf-8 -*-
-# Configuration for the Puppet Master.
+# Configuration for a Puppet Master.
 
-class role::puppetmaster (Array[String] $puppetserver_gems) {
-  include puppetdb
-  include puppetdb::master::config
-  include r10k::config
-  include r10k::webhook
-  include r10k::webhook::config
-  include role::puppetmaster::hiera_eyaml
-  include logrotate
+class role::puppetmaster (
+  Array[String] $classes,
+  Array[String] $gems,
+  Hash          $logrotate_rules,
+  Array[String] $packages,
+  Array[String] $puppetserver_gems,
+  Hash          $r10k_deploy_settings,
+  String[1]     $r10k_remote,
+  Boolean       $r10k_webhook_enable_ssl,
+  String[1]     $r10k_webhook_group,
+  String[1]     $r10k_webhook_token,
+  String[1]     $r10k_webhook_user,
+  Boolean       $r10k_webhook_use_mcollective,
+  Boolean       $r10k_webhook_protected,
+) {
+  $classes.each |String $cls| {
+    include $cls
+  }
 
-  # Ruby gems for the puppetserver embedded Ruby environment.
+  class { 'logrotate':
+    rules => $logrotate_rules,
+  }
+
+  class { 'r10k::config':
+    deploy_settings => $r10k_deploy_settings,
+    remote          => $r10k_remote,
+  }
+
+  class { 'r10k::webhook':
+    use_mcollective => $r10k_webhook_use_mcollective,
+    group           => $r10k_webhook_group,
+    user            => $r10k_webhook_user,
+  }
+
+  class { 'r10k::webhook::config':
+    enable_ssl      => $r10k_webhook_enable_ssl,
+    use_mcollective => $r10k_webhook_use_mcollective,
+    gitlab_token    => $r10k_webhook_token,
+    protected       => $r10k_webhook_protected,
+  }
+
+  ensure_packages($packages, {ensure => installed})
+
+  $gems.each |String $gem| {
+    package { "gem_${gem}":
+      ensure   => installed,
+      name     => $gem,
+      provider => 'gem',
+    }
+  }
+
   $puppetserver_gems.each |String $pkg| {
     package { "puppetserver_gem_${pkg}":
       ensure   => installed,
-      provider => puppetserver_gem,
       name     => $pkg,
+      provider => 'puppetserver_gem',
     }
   }
 }
