ac38fc9590bda5816f719861d30b54f755ed769a
Kenyon Ralph <kenyon@kenyonralph.com>
2020-12-05 01:49:30 -0800

    make sure all hosts can locally resolve their own names
    This way sudo doesn't complain when the system's own hostname can't be
    resolved due to a network or DNS outage, as happened with Vultr
    recently.

--- local-modules/mysite/manifests/fqdn_in_etc_hosts.pp
+++ local-modules/mysite/manifests/fqdn_in_etc_hosts.pp
@@ -1,6 +1,6 @@
 class mysite::fqdn_in_etc_hosts {
   host { $trusted['certname']:
     host_aliases => $trusted['hostname'],
-    ip           => '127.0.1.1',
+    ip           => $facts['networking']['ip6'],
   }
 }
