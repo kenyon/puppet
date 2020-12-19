c9e8eafa579c8f37db062020cd482deb44230c41
Kenyon Ralph <kenyon@kenyonralph.com>
2020-12-18 23:20:17 -0800

    mail: make the sasl_passwd content Sensitive
    So that the plaintext password doesn't appear in logs.

--- local-modules/mysite/manifests/mail.pp
+++ local-modules/mysite/manifests/mail.pp
@@ -1,6 +1,6 @@
 # My mail configuration.
 
-class mysite::mail {
+class mysite::mail (Sensitive[String[1]] $sasl_passwd_hash) {
   # darwin has its own special configuration
   unless $trusted['hostname'] == 'darwin' {
     include postfix
@@ -11,10 +11,8 @@ class mysite::mail {
       }
     }
 
-    lookup('postfix_hashes', Hash, 'hash', {}).each |$key, $value| {
-      postfix::hash { $key:
-        * => $value,
-      }
+    postfix::hash { '/etc/postfix/sasl/sasl_passwd':
+      content => $sasl_passwd_hash,
     }
   }
 }
