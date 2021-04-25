35e875959e0416308c61c968edd7b2c47a123c04
Kenyon Ralph <kenyon@kenyonralph.com>
2021-04-24 18:02:23 -0700

    homeserver: configure avahi to provide APIPA
    Devices are querying the DNS server on 169.254/16 addresses. Need the
    homeserver to have an address from that range so that it will accept
    those queries.

---
+++ local-modules/profile/manifests/avahi.pp
@@ -0,0 +1,48 @@
+# @summary Configure avahi
+class profile::avahi {
+  package { 'avahi-autoipd':
+    ensure => installed,
+  }
+
+  # Disable the DHCP hook actions.
+  [
+    '/etc/dhcp/dhclient-enter-hooks.d/avahi-autoipd',
+    '/etc/dhcp/dhclient-exit-hooks.d/zzz_avahi-autoipd',
+  ].each |Stdlib::Absolutepath $file| {
+    file { $file:
+      mode    => 'a-x',
+      require => Package['avahi-autoipd'],
+    }
+  }
+
+  # Disable the route-change hook scripts.
+  [
+    '/etc/network/if-up.d/avahi-autoipd',
+    '/etc/network/if-down.d/avahi-autoipd',
+  ].each |Stdlib::Absolutepath $file| {
+    file { $file:
+      mode    => 'a-x',
+      require => Package['avahi-autoipd'],
+    }
+  }
+
+  systemd::unit_file { 'avahi-autoipd@.service':
+    content => @(EOF)
+      [Unit]
+      Description=Avahi IPv4LL network address configuration daemon
+
+      [Service]
+      ExecStart=/usr/sbin/avahi-autoipd --force-bind %I
+      ExecReload=/usr/sbin/avahi-autoipd --refresh %I
+      ExecStop=/usr/sbin/avahi-autoipd --kill %I
+
+      [Install]
+      WantedBy=network.target
+      |EOF
+  }
+  ~> service { "avahi-autoipd@${facts['networking']['primary']}":
+    ensure  => running,
+    enable  => true,
+    require => Package['avahi-autoipd'],
+  }
+}
