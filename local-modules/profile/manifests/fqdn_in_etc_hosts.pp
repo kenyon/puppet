4a1f5da649ca8970076d6cd1b1e9b8dfc2f73d08
Kenyon Ralph <kenyon@kenyonralph.com>
2021-05-02 10:51:15 -0700

    manage DNS resolution with systemd-resolved

--- local-modules/profile/manifests/fqdn_in_etc_hosts.pp
+++
@@ -1,8 +0,0 @@
-# @summary Ensure that the FQDN is in /etc/hosts
-#   This is the standard Debian way to do it. See hosts(5).
-class profile::fqdn_in_etc_hosts {
-  host { $trusted['certname']:
-    host_aliases => $trusted['hostname'],
-    ip           => $facts['networking']['ip6'],
-  }
-}
