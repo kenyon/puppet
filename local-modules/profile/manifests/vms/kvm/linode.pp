85c6e446ffeb60804dd6e284a5cdf13e067468dc
Kenyon Ralph <kenyon@kenyonralph.com>
2021-10-14 00:01:25 -0700

    linode, darwin: install longview agent

--- local-modules/profile/manifests/vms/kvm/linode.pp
+++ local-modules/profile/manifests/vms/kvm/linode.pp
@@ -1,4 +1,38 @@
 # @summary configure things specific to Linodes
 #   See also: profile::vms::kvm
 #
-class profile::vms::kvm::linode {}
+# @param longview_api_key
+#   Longview API key.
+#
+class profile::vms::kvm::linode (
+  String[1] $longview_api_key,
+) {
+  apt::source { 'linode-longview':
+    location => 'https://apt-longview.linode.com/',
+    release  => $facts['os']['distro']['codename'],
+    repos    => 'main',
+    key      => {
+      id     => 'D12F8D2C47B4A16917C9A040BED67E64325A043E',
+      source => 'https://apt-longview.linode.com/linode.gpg',
+    }
+  }
+  -> package { 'linode-longview':
+    ensure => installed,
+  }
+  -> file { '/etc/linode':
+    ensure => directory,
+  }
+  -> file { '/etc/linode/longview.key':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0400',
+    content => @("EOT"),
+      ${longview_api_key}
+      | EOT
+  }
+  -> service { 'longview':
+    ensure => running,
+    enable => true,
+  }
+}
