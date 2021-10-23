fc3032bffb8686d3aee143a509afe1d4256a5252
Kenyon Ralph <kenyon@kenyonralph.com>
2021-10-23 01:02:49 -0700

    darwin decommissioned

--- local-modules/profile/manifests/mail.pp
+++
@@ -1,7 +0,0 @@
-# @summary My mail configuration
-class profile::mail {
-  # darwin is the main mail server and so has its own special configuration.
-  unless $trusted['hostname'] == 'darwin' {
-    include postfix
-  }
-}
