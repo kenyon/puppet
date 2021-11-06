4dab9428c137eaf67e33bb0d7a46b0d6a963838a
Kenyon Ralph <kenyon@kenyonralph.com>
2021-11-06 00:34:59 -0700

    mail: correctly mark the sasl_passwd content Sensitive

---
+++ local-modules/profile/manifests/mail.pp
@@ -0,0 +1,10 @@
+# @summary Manage mail config
+#
+# @param postfix_sasl_passwd_content Content of `/etc/postfix/sasl/sasl_passwd`
+class profile::mail (
+  String[1] $postfix_sasl_passwd_content,
+) {
+  postfix::hash { '/etc/postfix/sasl/sasl_passwd':
+    content => Sensitive("${postfix_sasl_passwd_content}\n"),
+  }
+}
