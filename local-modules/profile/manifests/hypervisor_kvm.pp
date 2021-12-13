9ca388ba4919add378f22bd5437de0c0aa20f8cd
Kenyon Ralph <kenyon@kenyonralph.com>
2021-12-12 18:29:59 -0800

    make hypervisor_kvm a proper self-contained profile

--- local-modules/profile/manifests/hypervisor_kvm.pp
+++ local-modules/profile/manifests/hypervisor_kvm.pp
@@ -1,20 +1,15 @@
-# @summary Configure things specific to a machine running a KVM hypervisor.
-#
-# @param file_capabilities
-#   File capabilities required for KVM functionality. See capabilities(7).
-#
-# @param packages
-#   Packages required for KVM functionality.
-#
-class profile::hypervisor_kvm (
-  Array[String[1]] $packages,
-  Hash $file_capabilities = {},
-) {
-  $file_capabilities.each |$path, $attrs| {
-    file_capability { $path:
-      * => $attrs,
+# @summary Configure things specific to a machine running a KVM hypervisor
+class profile::hypervisor_kvm {
+  [
+    'libosinfo-bin',
+    'virtinst',
+  ].each |String[1] $pkg| {
+    package { $pkg:
+      ensure => installed,
     }
   }
 
-  ensure_packages($packages, {ensure => installed})
+  file_capability { '/usr/lib/qemu/qemu-bridge-helper':
+    capability => 'cap_net_admin+ep',
+  }
 }
