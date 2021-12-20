832597dc0a6398eeb0af8bd1ac5736ba3740d8df
Kenyon Ralph <kenyon@kenyonralph.com>
2021-12-19 20:05:54 -0800

    mail: consolidate code in profile

--- local-modules/profile/manifests/mail.pp
+++ local-modules/profile/manifests/mail.pp
@@ -4,6 +4,8 @@
 class profile::mail (
   String[1] $postfix_sasl_passwd_content,
 ) {
+  include postfix
+
   postfix::hash { '/etc/postfix/sasl/sasl_passwd':
     content => Sensitive("${postfix_sasl_passwd_content}\n"),
   }
