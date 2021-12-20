51cee12047777aa5e3375de6a9859344106889b7
Kenyon Ralph <kenyon@kenyonralph.com>
2021-12-19 20:20:57 -0800

    multitail: handle FreeBSD path

--- local-modules/profile/manifests/multitail.pp
+++ local-modules/profile/manifests/multitail.pp
@@ -1,9 +1,14 @@
 # @summary Manage multitail
 class profile::multitail {
+  $freebsd_path = $facts['os']['family'] ? {
+    'FreeBSD' => '/usr/local',
+    default   => undef,
+  }
+
   package { 'multitail':
     ensure => installed,
   }
-  -> file { '/etc/multitail.conf':
+  -> file { "${freebsd_path}/etc/multitail.conf":
     ensure  => file,
     content => file("${module_name}/etc/multitail.conf"),
   }
