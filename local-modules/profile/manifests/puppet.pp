d4819a6dbffa2936e2cbbb1797bac55b7f57f641
Kenyon Ralph <kenyon@kenyonralph.com>
2021-12-23 21:29:18 -0800

    configure puppet.conf for FreeBSD
    puppetlabs-puppet_agent is not designed for non-Puppetlabs All-in-One
    agent packages, so it doesn't work with the FreeBSD port of puppet
    agent.

--- local-modules/profile/manifests/puppet.pp
+++ local-modules/profile/manifests/puppet.pp
@@ -1,6 +1,22 @@
 # @summary Manage puppet agent
 class profile::puppet {
-  include puppet_agent
+  if $facts['os']['family'] == 'FreeBSD' {
+    ini_setting { 'puppet agent splay':
+      path    => '/usr/local/etc/puppetlabs/puppet/puppet.conf',
+      section => 'agent',
+      setting => 'splay',
+      value   => 'true',
+    }
+
+    ini_setting { 'puppet show_diff':
+      path    => '/usr/local/etc/puppetlabs/puppet/puppet.conf',
+      section => 'main',
+      setting => 'show_diff',
+      value   => 'true',
+    }
+  } else {
+    include puppet_agent
+  }
 
   $freebsd_path = $facts['os']['family'] ? {
     'FreeBSD' => '/usr/local',
