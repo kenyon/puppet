e1ed8e6160c7556ccf4bdda18fb2f17b2753b6d0
Kenyon Ralph <kenyon@kenyonralph.com>
2024-05-11 15:43:54 -0700

    smokeping: enable unsafe legacy renegotiation for curl's openssl

---
+++ local-modules/profile/manifests/smokeping.pp
@@ -0,0 +1,31 @@
+# @summary Smokeping configuration
+class profile::smokeping {
+  # Needed because the QC VPN target, probed by curl, doesn't support secure
+  # renegotiation. Unsafe renegotiation is disabled by default in OpenSSL 3.
+  # https://stackoverflow.com/a/72245418/124703
+  file { '/etc/smokeping/openssl.cnf':
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => @(EOT),
+      openssl_conf = openssl_init
+
+      [openssl_init]
+      ssl_conf = ssl_sect
+
+      [ssl_sect]
+      system_default = system_default_sect
+
+      [system_default_sect]
+      Options = UnsafeLegacyRenegotiation
+      | EOT
+  }
+  -> systemd::dropin_file { 'environment.conf':
+    unit    => 'smokeping.service',
+    content => @(EOT),
+      [Service]
+      Environment=OPENSSL_CONF=/etc/smokeping/openssl.cnf
+      | EOT
+  }
+}
