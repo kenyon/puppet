685cfd2cda5ff0b31edf09396517daef3bb38fef
Kenyon Ralph <kenyon@kenyonralph.com>
2025-02-25 22:27:37 -0800

    main: use namespaced stdlib::ensure_packages()

--- manifests/main.pp
+++ manifests/main.pp
@@ -1,7 +1,7 @@
 util::lookup_filter('classes').include
 
-ensure_packages(util::lookup_filter('packages'), { ensure => installed })
-ensure_packages(util::lookup_filter('packages_absent'), { ensure => purged })
+stdlib::ensure_packages(util::lookup_filter('packages'), { ensure => installed })
+stdlib::ensure_packages(util::lookup_filter('packages_absent'), { ensure => purged })
 
 lookup('files', Hash, 'hash', {}).each |$key, $value| {
   file { $key:
