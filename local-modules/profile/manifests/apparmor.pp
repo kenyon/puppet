6d4916341c6e22f1545c119ebc69a9ddb1b33dcb
Kenyon Ralph <kralph@qti.qualcomm.com>
2025-11-21 00:27:17 -0800

    apparmor: install on Linodes too
    Not using special Linode kernels anymore, those were deprecated, so
    AppArmor should work fine on Linodes.

--- local-modules/profile/manifests/apparmor.pp
+++ local-modules/profile/manifests/apparmor.pp
@@ -5,24 +5,21 @@
 class profile::apparmor (
   Hash $profile_files = {},
 ) {
-  # Linode kernels apparently don't support apparmor.
-  unless util::is_linode() {
-    package { 'apparmor':
-      ensure => installed,
-    }
-    -> service { 'apparmor':
-      enable => true,
-    }
+  package { 'apparmor':
+    ensure => installed,
+  }
+  -> service { 'apparmor':
+    enable => true,
+  }
 
-    $profile_files.each |$key, $value| {
-      file { "/etc/apparmor.d/local/${key}":
-        ensure => file,
-        owner  => root,
-        group  => root,
-        mode   => '0644',
-        notify => Service['apparmor'],
-        *      => $value,
-      }
+  $profile_files.each |String[1] $key, Hash $value| {
+    file { "/etc/apparmor.d/local/${key}":
+      ensure => file,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      notify => Service['apparmor'],
+      *      => $value,
     }
   }
 }
